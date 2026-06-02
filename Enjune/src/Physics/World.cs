14c758ac2ac8ac85e612e368db608db7c8f69b13
udnek <gleb.dudorov@gmail.com>
2026-06-02 14:03:45 +0300

    Todos

--- Enjune/src/Physics/World.cs
+++ Enjune/src/Physics/World.cs
@@ -3,6 +3,7 @@ using Enjune.Physics.Manager;
 
 namespace Enjune.Physics;
 
+// TODO what is the purpose apart from composing if they do not interact with each other. I idk anyway
 public class World
 {
     public static readonly ArchetypeManager ArchetypeManager = new ArchetypeManager();
