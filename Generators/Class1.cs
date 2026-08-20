dac0e2818be5fe463cc1e1727201d52fb41cb630
JUP1TER <d.savinykh@edu.centraluniversity.ru>
2026-08-20 12:24:58 +0300

    pre-merge

---
+++ Generators/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace Generators
+{
+    [Generator]
+    public class EcsQueryGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
